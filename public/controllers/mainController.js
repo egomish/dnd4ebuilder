b295bf6646b4cb49b3d60e05daca131c5afaacc0
ekoenitz <33014529+ekoenitz@users.noreply.github.com>
2018-02-08 21:22:54 -0800

    Added data bindings

--- public/controllers/mainController.js
+++ public/controllers/mainController.js
@@ -1,4 +1,19 @@
 var app = angular.module('app', []);
-app.controller('mainController', ['$scope', '$http', 'characterInfo', function($scope, $http, characterInfo) {
-
-}]);
+app.controller('mainController', ['$scope', '$http', function($scope, $http) {
+	$scope.ddch = {
+		level0: {},
+		level1: {},
+		level2: {},
+		level3: {},
+		level4: {},
+		level5: {},
+		level6: {},
+		level7: {},
+		level8: {},
+		level9: {},
+		level10: {}
+	};
+	$scope.log = function() {
+		console.log($scope.ddch);
+	}
+}]);	
