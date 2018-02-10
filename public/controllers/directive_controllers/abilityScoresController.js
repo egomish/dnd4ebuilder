f0a8370804df3af1f75705a74fe2b32b272922e3
cbhenson <cbhenson@ucsc.edu>
2018-02-09 23:17:22 -0800

    Base Score display
    Left side of Ability directive box

--- public/controllers/directive_controllers/abilityScoresController.js
+++ public/controllers/directive_controllers/abilityScoresController.js
@@ -1,3 +1,22 @@
 app.controller('abilityScoresController', ['$scope', '$http', function($scope, $http) {
-    $scope.test = 'hi from ability scores controller';
+    $scope.initScores = function() {
+    	$scope.ddch.level0.str_score = [
+			{ base_score: 10, abil_mod: 0, lvl_mod: 0 }
+		]
+		$scope.ddch.level0.con_score = [
+			{ base_score: 10, abil_mod: 0, lvl_mod: 0 }
+		]
+		$scope.ddch.level0.dex_score = [
+			{ base_score: 10, abil_mod: 0, lvl_mod: 0 }
+		]
+		$scope.ddch.level0.int_score = [
+			{ base_score: 10, abil_mod: 0, lvl_mod: 0 }
+		]
+		$scope.ddch.level0.wis_score = [
+			{ base_score: 10, abil_mod: 0, lvl_mod: 0 }
+		]
+		$scope.ddch.level0.cha_score = [
+			{ base_score: 10, abil_mod: 0, lvl_mod: 0 }
+		]
+	}
 }]);
