12ba6dd7797953815b80ad4ad19d1f9248243739
ekoenitz <33014529+ekoenitz@users.noreply.github.com>
2018-02-12 19:10:50 -0800

    Made this branch's mainController match the dev branch's

--- public/controllers/mainController.js
+++ public/controllers/mainController.js
@@ -47,22 +47,49 @@ app.controller('mainController', ['$scope', '$http', function($scope, $http) {
                     desc: 'Can move 1 less when being forced to move.'
                 }
             ],
-            abilityScores: []
+            abilityScores: [
+                { 
+                    base_str: 10,
+                    base_con: 10,
+                    base_dex: 10,
+                    base_int: 10,
+                    base_wis: 10,
+                    base_cha: 10
+                }
+            ]
         },
         level1: {
             ddclass: "",
             skillTrainings: [],
-            classFeatures: [],
-            ddfeat: "",
+            classFeatures: [
+                {
+                    name: "Combat Leader",
+                    desc: "You and allies within 10 that see and hear you gain +2 to initiative."
+                },
+                {
+                    name: "Tactical Presence",
+                    desc: "Ally you can see that spends an action point to attack gains bonus to attack: 1/2 int mod."
+                },
+                {
+                    name: "Inspiring Word",
+                    desc: "Use inspiring word as an encounter (special) power, minor action."
+                }
+            ],
             atwillPower1: "",
             atwillPower2: "",
             encounterPower: "",
             dailyPower: "",
-            ddfeat: ""
+            ddfeat: { 
+                name: "Toughness", 
+                desc: "Gain 5 additional hit points per tier" 
+            }
         },
         level2: {
             utilityPower: "",
-            ddfeat: ""
+            ddfeat: { 
+                name: "", 
+                desc: "" 
+            }
         },
         level3: {
             encounterPower: ""
@@ -70,14 +97,20 @@ app.controller('mainController', ['$scope', '$http', function($scope, $http) {
         level4: {
             abilityScoreBonus1: "",
             abilityScoreBonus2: "",
-            ddfeat: ""
+            ddfeat: { 
+                name: "", 
+                desc: "" 
+            }
         },
         level5: {
             dailyPower: ""
         },
         level6: {
             utilityPower: "",
-            ddfeat: ""
+            ddfeat: { 
+                name: "", 
+                desc: "" 
+            }
         },
         level7: {
             encounterPower: ""
@@ -85,34 +118,46 @@ app.controller('mainController', ['$scope', '$http', function($scope, $http) {
         level8: {
             abilityScoreBonus1: "",
             abilityScoreBonus2: "",
-            ddfeat: ""
+            ddfeat: { 
+                name: "", 
+                desc: "" 
+            }
         },
         level9: {
             dailyPower: ""
         },
         level10: {
             utilityPower: "",
-            ddfeat: ""
+            ddfeat: { 
+                name: "", 
+                desc: "" 
+            }
         },
         calculatedValues: {
             halfLevel: 0,
+            movement: {
+                base: 5,
+                armor: 0,
+                item: 0,
+                misc: 0
+            },
             initiative: {
                 dex: 0,
                 misc: 0
             },
             abilityScores: {
-                strTotal: 0,
-                conTotal: 0,
-                dexTotal: 0,
-                intTotal: 0,
-                wisTotal: 0,
-                chaTotal: 0,
+                strTotal: 10,
+                conTotal: 10,
+                dexTotal: 10,
+                intTotal: 10,
+                wisTotal: 10,
+                chaTotal: 10,
                 strMod: 0,
                 conMod: 0,
                 dexMod: 0,
                 intMod: 0,
                 wisMod: 0,
-                chaMod: 0,
+                chaMod: 0
             },
             healthAndSavingThrows: {
                 maxHP: 0,
@@ -180,4 +225,4 @@ app.controller('mainController', ['$scope', '$http', function($scope, $http) {
 		console.log($scope.ddch);
         $scope.ddch.level0.healthAndSavingThrows.maxHP = "24";
 	}
-}]);	
+}]);
