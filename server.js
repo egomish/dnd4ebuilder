2a106bfac134c6a205e9ecb356764112ce56254e
ekoenitz <33014529+ekoenitz@users.noreply.github.com>
2018-02-25 11:54:26 -0800

    Made a function for calculating ability scores.

--- server.js
+++ server.js
@@ -37,6 +37,37 @@ app.post('/uploadfile', function(req, res) {
 	}
 });
 
+function getAbilityBonus(ddch, ability) {
+  var bonus = 0;
+  if (ddch.abilityBonus1 === ability || ddch.abilityBonus2 === ability) {
+    bonus = 2;
+  }
+  return bonus;
+}
+
+function calcAbilityScores(ddch) {
+  var cv = {};
+  var abs = ddch.level0.abilityScores[0];
+  cv.strTotal = abs.baseStr + getAbilityBonus(ddch, "Strength");
+  cv.strMod = Math.floor((cv.strTotal - 10) / 2);
+  
+  cv.conTotal = abs.baseCon + getAbilityBonus(ddch, "Constitution");
+  cv.conMod = Math.floor((cv.conTotal - 10) / 2);
+  
+  cv.dexTotal = abs.baseDex + getAbilityBonus(ddch, "Dexterity");
+  cv.dexMod = Math.floor((cv.dexTotal - 10) / 2);
+  
+  cv.intTotal = abs.baseInt + getAbilityBonus(ddch, "Intelligence");
+  cv.intMod = Math.floor((cv.intTotal - 10) / 2);
+  
+  cv.wisTotal = abs.baseWis + getAbilityBonus(ddch, "Wisdom");
+  cv.wisMod = Math.floor((cv.wisTotal - 10) / 2);
+  
+  cv.chaTotal = abs.baseCha + getAbilityBonus(ddch, "Charisma");
+  cv.chaMod = Math.floor((cv.chaTotal - 10) / 2);
+  return cv;
+}
+
 app.post('/calculateValues', function(req, res) {
   var ddch = req.body;
   var cv = {}; //cv = Calculated Values
@@ -46,14 +77,13 @@ app.post('/calculateValues', function(req, res) {
   cv.raceFeatures = "Race Stuff";
   cv.movement = "Movement Stuff";
   cv.initiative = "Initiative Stuff";
-  cv.abilityScores = "Ability Stuff";
+  cv.abilityScores = calcAbilityScores(ddch);
   cv.healthAndSavingThrows = "Health Stuff";
   cv.skills = "Skills Stuff";
   cv.defenses = "Defense Stuff";
   cv.weaponProficiencies = "Weapon Stuff";
   cv.powers = "Power Stuff";
   
-  console.log(cv);
   ddch.calculatedValues = cv;
   res.json(ddch);
 });
