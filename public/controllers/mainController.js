82231903cda37e1649139e92898262ec1a47b2da
unknown <akellar@ucsc.edu>
2018-02-24 18:06:27 -0800

    removed a custom directive that is no longer needed

--- public/controllers/mainController.js
+++ public/controllers/mainController.js
@@ -1,22 +1,4 @@
 var app = angular.module('app', []);
-app.directive("fileread", [function () {
-    return {
-        scope: {
-            fileread: "="
-        },
-        link: function (scope, element, attributes) {
-            element.bind("change", function (changeEvent) {
-                var reader = new FileReader();
-                reader.onload = function (loadEvent) {
-                    scope.$apply(function () {
-                        scope.fileread = loadEvent.target.result;
-                    });
-                }
-                reader.readAsDataURL(changeEvent.target.files[0]);
-            });
-        }
-    }
-}]);
 app.controller('mainController', ['$scope', '$http', function($scope, $http) {
     $scope.ddch = {
         characterLevel: 1,
