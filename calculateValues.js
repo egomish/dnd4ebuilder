2dd15f1158ec72fe45eff95614749375130ba4f9
Emma Gomish <egomish@ucsc.edu>
2018-03-07 07:02:49 -0800

    Refactored parameters more.

--- calculateValues.js
+++ calculateValues.js
@@ -1,13 +1,15 @@
 var con;
+var ddch;
 var res;
 
-module.exports.getEverythingFromDatabase = function (db, ddch, response) {
+module.exports.getEverythingFromDatabase = function (db, ddchar, response) {
     con = db;
+    ddch = ddchar;
     res = response;
-    getSize(ddch);
+    getSize();
 }
 
-function getSize (ddch) {
+function getSize () {
   var racename = ddch.level0.ddrace;
   var query = "SELECT size FROM races WHERE name LIKE '" + racename + "';";
   con.query(query, function (err, result) {
@@ -16,11 +18,11 @@ function getSize (ddch) {
       } else {
         ddch.calculatedValues.size = result[0].size;
       }
-      getACBonus(ddch);
+      getACBonus();
   });
 }
 
-function getACBonus (ddch) {
+function getACBonus () {
   var classname = ddch.level1.ddclass;
   var query = "SELECT acBonus FROM classes WHERE name LIKE '" + classname + "';";
   con.query(query, function (err, result) {
@@ -29,11 +31,11 @@ function getACBonus (ddch) {
       } else {
         ddch.calculatedValues.acBonus = result[0].acBonus;
       }
-      completeResponse(ddch);
+      completeResponse();
   });
 }
 
-function completeResponse (ddch) {
+function completeResponse () {
     res.json(ddch);
 }
 
