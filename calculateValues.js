b6ef3640721064bc7df4630e8991c5f4876fab4c
Emma Gomish <egomish@ucsc.edu>
2018-03-10 09:44:30 -0800

    added more to database accessing  - handled more fields in calculateValues  - fixed inconsistent indentation style  - querytest: added template functions for querying database  - queries: test plan for queries

--- calculateValues.js
+++ calculateValues.js
@@ -10,29 +10,49 @@ module.exports.getEverythingFromDatabase = function (db, ddchar, response) {
 }
 
 function getSize () {
-  var racename = ddch.level0.ddrace;
-  var query = "SELECT size FROM races WHERE name LIKE '" + racename + "';";
-  con.query(query, function (err, result) {
+    var racename = ddch.level0.ddrace;
+    var query = "SELECT size FROM races WHERE name LIKE '" + racename + "';";
+    con.query(query, function (err, result) {
       if (err) {
-        database_error(err, query);
+          database_error(err, query);
       } else {
-        ddch.calculatedValues.size = result[0].size;
+          ddch.calculatedValues.size = result[0].size;
       }
-      getACBonus();
+      getRaceFeatures();
   });
 }
 
-function getACBonus () {
-  var classname = ddch.level1.ddclass;
-  var query = "SELECT acBonus FROM classes WHERE name LIKE '" + classname + "';";
-  con.query(query, function (err, result) {
+function getRaceFeatures () {
+    ddch.calculatedValues.raceFeatures = [];
+    var racename = ddch.level0.ddrace;
+    var query = "SELECT name, benefit FROM features " 
+              + "WHERE origin LIKE '" + racename + "';";
+    con.query(query, function (err, result) {
       if (err) {
-        database_error(err, query);
+          database_error(err, query);
       } else {
-        ddch.calculatedValues.acBonus = result[0].acBonus;
+          for (tuple in result) {
+              var feature = {};
+              feature.name = result[tuple].name;
+              feature.desc = result[tuple].benefit;
+              ddch.calculatedValues.raceFeatures[tuple] = feature;
+          }
       }
-      completeResponse();
-  });
+      getACBonus();
+    });
+}
+
+function getACBonus () {
+    var classname = ddch.level1.ddclass;
+    var query = "SELECT acBonus FROM classes WHERE name LIKE '" + classname + "';";
+    con.query(query, function (err, result) {
+        if (err) {
+            database_error(err, query);
+        } else {
+            ddch.calculatedValues.acBonus = result[0].acBonus;
+        }
+        completeResponse();
+    });
 }
 
 function completeResponse () {
@@ -40,6 +60,6 @@ function completeResponse () {
 }
 
 function database_error (err, query) {
-  console.log("Database query \"" + query + "\" failed.");
+    console.log("Database query \"" + query + "\" failed.");
 }
 
--- querytest.js
+++ querytest.js
@@ -12,37 +12,91 @@ con.connect(function (err) {
         console.log("Connection to database failed")
     } else {
         console.log("Connected to database");
+        doQueries();
     }
 });
 
-doQueries();
 
 function doQueries () {
-    doQueryOne();
+    doQueryDebug();
 }
 
-function doQueryOne () {
+//debug example: get all attributes from every tuple
+//get every attribute from all classes
+function doQueryDebug () {
     var query = "SELECT * FROM classes;";
     con.query(query, function (err, result) {
         if (err) {
             database_error(err, query);
         } else {
-            console.log(result[0]);
+            console.log("\n" + query);
+            for (tuple in result) {
+                for (attr in result[tuple]) {
+                    console.log(attr + ": " + result[tuple][attr]);
+                }
+            }
         }
+        doQueryComplex();
     });
-    doQueryTwo();
 }
 
-function doQueryTwo () {
-    var query = "SELECT * FROM races;";
+//complex example: get multiple attributes from multiple tuples
+//get all racial features from one race
+function doQueryComplex () {
+    var racename = "Halfling";
+    var query = "SELECT name, benefit "
+              + "FROM features "
+              + "WHERE origin LIKE '" + racename + "';";
     con.query(query, function (err, result) {
         if (err) {
             database_error(err, query);
         } else {
-            console.log(result[0]);
+            console.log("\n" + query);
+            for (tuple in result) {
+                console.log(result[tuple].name +", " + result[tuple].benefit);
+            }
         }
+        doQuerySimple();
     });
-    finishQueries();
+}
+
+//simple example: get one attribute from one tuple
+function doQuerySimple () {
+    var classname = "Rogue";
+    var query = "SELECT level1HP "
+              + "FROM classes "
+              + "WHERE name LIKE '" + classname + "';";
+    con.query(query, function (err, result) {
+        if (err) {
+            database_error(err, query);
+        } else {
+            console.log("\n" + query);
+            console.log("level1HP: " + result[0]["level1HP"]);
+        }
+        doQueryStrengthTotal();
+    });
+}
+
+function doQueryStrengthTotal () {
+    var racename = "Dragonborn";
+    var query = "SELECT abilityScoreBonus1, abilityScoreBonus2 "
+              + "FROM races "
+              + "WHERE name LIKE '" + racename + "';";
+    con.query(query, function (err, result) {
+        if (err) {
+            database_error(err, query);
+        } else {
+            var bonus1 = result[0]["abilityScoreBonus1"];
+            var bonus2 = result[0]["abilityScoreBonus2"];
+            console.log("\n" + query);
+            console.log("bonus1: " + bonus1);
+            console.log("bonus2: " + bonus2);
+            if (bonus1 === "+2 Strength" || bonus2 === "+2 Strength") {
+                console.log("+2 to strTotal");
+            }
+        }
+        finishQueries();
+    });
 }
 
 function finishQueries () {
