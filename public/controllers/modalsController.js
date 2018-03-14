9cdfb9c5502460aa2f6ff79c02ca00d316512645
Austin Kellar <austinkellar@Mac-Users-MacBook-7.local>
2018-03-13 20:32:55 -0700

    removed blank option from race modal selection

--- public/controllers/modalsController.js
+++ public/controllers/modalsController.js
@@ -2,12 +2,11 @@ app.controller('modalsController', ['$scope', '$http', function($scope, $http) {
     $scope.getRaces = function() {
         $http.get('/races').then(function(response) {
           $scope.races = response.data;
+          $scope.selectedRace = $scope.races[0].name;
       }, function (error) {
           console.log('Could not GET races from server');
       });
     };
-
-    $scope.selectedRace = undefined;
 
     $scope.changeRace = function() {
         for(var i=0; i<$scope.races.length; i++) {
