4b65f99438ac8bf6d87d85507727946130d6b73f
cbhenson <cbhenson@ucsc.edu>
2018-03-16 00:30:34 -0700

    get con and dex
    Added constitution and dexterity get functions.

--- calculateValues.js
+++ calculateValues.js
@@ -71,10 +71,77 @@ function getStrengthModifier () {
 
     ddch.calculatedValues.abilityScores.strMod = strmod;
     ddch.calculatedValues.skills[2][3] = strmod;
+    // make sure this is called before getConModifier
     ddch.calculatedValues.defenses.fortitude.abilityMod = strmod;
     //TODO: handle ddch.calculatedValues.weaponProficiencies[i].damageMod
-
+    getConTotal();
     getACBonus();
+}
+
+function getConTotal () {
+    var racename = ddch.level0.ddrace;
+    var basecon = ddch.level0.abilityScores.baseCon;
+    var query = "SELECT conBonus FROM races " 
+              + "WHERE name LIKE '" + racename + "';";
+    con.query(query, function (err, result) {
+      if (err) {
+          database_error(err, query);
+      } else {
+          for (tuple in result) {
+              conbonus = result[tuple].conBonus;
+              ddch.calculatedValues.abilityScores.conTotal = basecon;
+              ddch.calculatedValues.abilityScores.conTotal += conbonus;
+              ddch.calculatedValues.healthAndSavingThrows.maxHP += ddch.calculatedValues.abilityScores.conTotal;
+          }
+      }
+      getConModifier();
+    });
+}
+
+function getConModifier () {
+    var contotal = ddch.calculatedValues.abilityScores.conTotal;
+    var conmod = getAbilMod(ddch.calculatedValues.abilityScores.conTotal);
+
+    ddch.calculatedValues.abilityScores.conMod = conmod;
+    ddch.calculatedValues.skills[6][3] = conmod;
+    if (conmod > ddch.calculatedValues.defenses.fortitude.abilityMod)
+      ddch.calculatedValues.defenses.fortitude.abilityMod = conmod;
+    ddch.calculatedValues.healthAndSavingThrows.surgesPerDay += conmod;
+
+    getDexTotal();
+}
+
+function getDexTotal () {
+    var racename = ddch.level0.ddrace;
+    var basedex = ddch.level0.abilityScores.baseDex;
+    var query = "SELECT dexBonus FROM races " 
+              + "WHERE name LIKE '" + racename + "';";
+    con.query(query, function (err, result) {
+      if (err) {
+          database_error(err, query);
+      } else {
+          for (tuple in result) {
+              dexbonus = result[tuple].dexBonus;
+              ddch.calculatedValues.abilityScores.dexTotal = basedex;
+              ddch.calculatedValues.abilityScores.dexTotal += dexbonus;
+          }
+      }
+      getDexModifier();
+    });
+}
+
+function getDexModifier () {
+    var dextotal = ddch.calculatedValues.abilityScores.dexTotal;
+    var dexmod = getAbilMod(ddch.calculatedValues.abilityScores.dexTotal);
+
+    ddch.calculatedValues.abilityScores.dexMod = dexmod;
+    ddch.calculatedValues.skills[0][3] = dexmod;
+    ddch.calculatedValues.skills[14][3] = dexmod;
+    ddch.calculatedValues.skills[16][3] = dexmod;
+    ddch.calculatedValues.defenses.ac.abilityMod = dexmod;
+    ddch.calculatedValues.defenses.reflex.abilityMod = dexmod;
+    //TODO: handle ddch.calculatedValues.weaponProficiencies[i].damageMod
+    
 }
 
 function getACBonus () {
